dafa923dbd93a39bff22766561ade57b0e397594
Julliana <jullianabinda@live.com>
2021-06-23 05:18:29 -0300

    adicionado readme

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -1,6 +1,12 @@
 
 public class Exemplo {
 	
+	public static void sayHelloManyTimes(int times) {
+		for (int i=0; i < times; i++) {
+			sayHello();
+		}
+	}
+	
 	public static void sayHello () {
 		System.out.println("Olá mundo!");
 	}
